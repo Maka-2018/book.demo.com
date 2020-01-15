7b3147c49de7fcaf5d6e91b182e33410a95722ec
amos2013@outlook.com <amos2013@outlook.com>
2020-01-15 22:33:51 +0100

    Modify PowerMockito inside the BookPatternJunitTest

--- book.demo.com/src/test/java/com/book/run/BookPatternJunitTest.java
+++ book.demo.com/src/test/java/com/book/run/BookPatternJunitTest.java
@@ -1,46 +1,37 @@
 package com.book.run;
 
-import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
-import org.mockito.Matchers;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.mockito.runners.MockitoJUnitRunner;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
 
-import com.book.entity.Book;
 
-@RunWith(MockitoJUnitRunner.class)
 @PrepareForTest(BookPattern.class)
 public class BookPatternJunitTest {
 	
 	@Mock
 	BookPattern nBookPattern;
 	
-	@Captor
-	ArgumentCaptor<Book> bookArgument = ArgumentCaptor.forClass(Book.class);
-	
 	@Before
 	public void setUp() throws Exception{
 		
-		MockitoAnnotations.initMocks(this);
-		nBookPattern = PowerMockito.mock(BookPattern.class);
+		 MockitoAnnotations.initMocks(this);
 
-
+		 this.nBookPattern = PowerMockito.mock(BookPattern.class);
 	}
 	
+	@SuppressWarnings({"static-access" })
 	@Test
 	public void test_main() throws Exception{
-		 final String[] strValue = {"",""};
-		 BookPattern.main(strValue);		 
+		 
+		String[] strValue = {"VALUE"};
+				
+		this.nBookPattern.main(strValue);
+		
+		Assert.assertNotNull(this.nBookPattern);	
 		 
 	}
 	
